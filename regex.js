b403a84c0f4eb45a2a7328f8f55cbe8da177f0a8
quiant <quiantthefox@gmail.com>
2023-12-01 09:19:01 +0100

    vowelsLowercase

---
+++ regex.js
@@ -0,0 +1,7 @@
+const string=`La rupture conventionnelle permet à l\'employeur et 
+au salarié en CDI de convenir d\'un commun accord des conditions de 
+rupture du contrat de travail qui les lie.`
+
+const vowelsLowercase = /[a,e,i,o,u]/g
+
+console.log(string.match(vowelsLowercase))
