53db2014c445aebb6b1f969eaf13d0cddf7e02dd
quiant <quiantthefox@gmail.com>
2023-12-01 10:38:46 +0100

    noVowelsLowercase

--- regex.js
+++ regex.js
@@ -2,6 +2,11 @@ const string=`La rupture conventionnelle permet à l\'employeur et
 au salarié en CDI de convenir d\'un commun accord des conditions de 
 rupture du contrat de travail qui les lie.`
 
-const vowelsLowercase = /[a,e,i,o,u]/g
 
-console.log(string.match(vowelsLowercase))
+// uniquement les voyelles
+const vowelsLowercase = /[a,e,i,o,u,y]/g
+// tout sauf les voyelles
+const noVowelsLowercase =/[^a,e,i,o,u,y]/g
+
+
+console.log(string.match(noVowelsLowercase))
